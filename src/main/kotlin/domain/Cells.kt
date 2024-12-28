556db61551bbb0e10738114e5edeaf7532bc4ecc
devtaebong <thk98k@gmail.com>
2024-12-28 20:29:01 +0900

    refactor: 일급컬렉션을 value class로 변경

--- src/main/kotlin/domain/Cells.kt
+++ src/main/kotlin/domain/Cells.kt
@@ -2,7 +2,8 @@ package domain
 
 import domain.strategy.MineCellGenerator
 
-data class Cells(private val cells: List<Cell>) {
+@JvmInline
+value class Cells(private val cells: List<Cell>) {
     companion object {
         fun of(mineCellGenerator: MineCellGenerator): Cells {
             val mineCells = mineCellGenerator.execute()
