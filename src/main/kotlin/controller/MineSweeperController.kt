ef8b8cfb1620f4f7b0d90c27f13ada3108eb0012
devtaebong <thk98k@gmail.com>
2024-12-29 17:46:10 +0900

    fix: Model로 변환을 Controller 계층에서 수행하도록 변경한다.

--- src/main/kotlin/controller/MineSweeperController.kt
+++ src/main/kotlin/controller/MineSweeperController.kt
@@ -1,9 +1,12 @@
 package controller
 
 import domain.Cells
+import domain.Col
+import domain.Coordinate
 import domain.MineBoard
 import domain.MineSweeperGame
 import domain.MineSweeperMetric
+import domain.Row
 import domain.strategy.MineCellGenerator
 import domain.strategy.RandomMineCellGenerator
 import view.InputView
@@ -21,18 +24,6 @@ class MineSweeperController {
         gameLoop(mineBoard)
     }
 
-    private fun gameLoop(mineBoard: MineBoard) {
-        val mineSweeperGame = MineSweeperGame(mineBoard)
-        while (mineSweeperGame.isContinueGame()) {
-            val requestOpenCoordinate = InputView.askMineCoordinate()
-            mineSweeperGame.openAdjacentCell(requestOpenCoordinate)
-            OutputView.showMineSweeperBoard(mineBoard)
-        }
-
-        val result = mineSweeperGame.getGameResult()
-        OutputView.showGameResult(result)
-    }
-
     private fun initializeMineSweeperMetric(): MineSweeperMetric {
         val mineBoardHeight = InputView.inputMineBoardHeight()
         val mineBoardWidth = InputView.inputMineBoardWidth()
@@ -42,5 +33,29 @@ class MineSweeperController {
             mineBoardWidth = mineBoardWidth,
             mineCount = mineCount,
         )
+    }
+
+    private fun gameLoop(mineBoard: MineBoard) {
+        val mineSweeperGame = MineSweeperGame(mineBoard)
+        while (mineSweeperGame.isContinueGame()) {
+            val requestOpenCoordinate = parseToCoordinateOrThrow(InputView.askMineCoordinate())
+            mineSweeperGame.openAdjacentCell(requestOpenCoordinate)
+            OutputView.showMineSweeperBoard(mineBoard)
+        }
+
+        val result = mineSweeperGame.getGameResult()
+        OutputView.showGameResult(result)
+    }
+
+    private fun parseToCoordinateOrThrow(value: String): Coordinate {
+        return value.replace(" ", "")
+            .split(",")
+            .also { require(it.size == 2) { INVALID_COORDINATE + value } }
+            .map { it.toIntOrNull() ?: throw IllegalArgumentException(INVALID_COORDINATE) }
+            .let { (row, col) -> Coordinate(Row(row), Col(col)) }
+    }
+
+    companion object {
+        private const val INVALID_COORDINATE = "두가지 숫자를 콤마(,)로 구분해서 입력해주세요 "
     }
 }
